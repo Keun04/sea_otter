d62d25736a30a50594297d090cfa6c18f74c7534
Keun04 <128891914+Keun04@users.noreply.github.com>
2024-12-02 21:09:53 +0900

    Feat: Day 페이지 기본 틀 제작

---
+++ client/src/pages/DayPage.js
@@ -0,0 +1,12 @@
+import NavBar from '../components/NavBar';
+import styles from './DayPage.module.css';
+
+function DayPage() {
+  return (
+    <div className={styles.background}>
+      <NavBar />
+    </div>
+  );
+}
+
+export default DayPage;
