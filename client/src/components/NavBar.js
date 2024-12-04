b26fa6a7706aed1cfecfafde05b8f51e7428e128
Keun04 <128891914+Keun04@users.noreply.github.com>
2024-12-04 20:08:18 +0900

    Design: Nav Item Hover 시 text 변경

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,19 +1,38 @@
 import { useNavigate } from 'react-router-dom';
 import Logo from '../assets/Logo.png';
 import styles from './NavBar.module.css';
+import { useState } from 'react';
 
 function NavBar() {
   const navigate = useNavigate();
 
   const handleClickNavItem = (title) => {
-    navigate(`/${title}`); /* 페이지 이동 */
+    navigate(`/${title}`);
   };
+
+  const menuItems = [
+    { id: 1, defaultText: 'Play', hoverText: 'Day' },
+    { id: 2, defaultText: 'Portfolio', hoverText: 'Night' },
+    { id: 3, defaultText: 'About us', hoverText: 'Sunset' },
+  ];
 
   return (
     <div className={styles.container}>
       <img src={Logo} className={styles.logo} onClick={() => handleClickNavItem('')} />
-      <ul className={styles.navList}>
-        <li className={styles.navItem}>
+      <div className={styles.navList}>
+        {menuItems.map((item) => (
+          <div className={styles.navItem}>
+            <span onClick={() => handleClickNavItem('')} className={`${styles.navLink} ${styles.defaultText}`}>
+              {item.defaultText}
+            </span>
+            <span onClick={() => handleClickNavItem('')} className={`${styles.navLink} ${styles.hoverText}`}>
+              {item.hoverText}
+            </span>
+          </div>
+        ))}
+      </div>
+
+      {/* <li className={styles.navItem}>
           <a onClick={() => handleClickNavItem('')} className={styles.navLink}>
             Play
           </a>
@@ -27,8 +46,7 @@ function NavBar() {
           <a onClick={() => handleClickNavItem('sunset')} className={styles.navLink}>
             About us
           </a>
-        </li>
-      </ul>
+        </li> */}
     </div>
   );
 }
