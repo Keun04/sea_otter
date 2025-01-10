829b91fce1a627e41a47c13109db643b17b3189c
Keun04 <128891914+Keun04@users.noreply.github.com>
2025-01-10 22:05:52 +0900

    Feat: gsap 설치 후 스크롤 개발 중
    스크롤이 아예 작동이 안되는 문제 고치는 중

--- client/src/pages/DayPage.js
+++ client/src/pages/DayPage.js
@@ -1,14 +1,31 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
 import NavBar from '../components/NavBar';
 import styles from './DayPage.module.css';
-import { useState } from 'react';
 import RoomModal from '../components/RoomModal';
 
 import GyIsland from '../assets/GyIsland.webp';
 import LIsland from '../assets/LIsland.webp';
 import GyRoomDay from '../assets/dayGY.png';
 
+gsap.registerPlugin(ScrollTrigger);
+
 function DayPage() {
   const [isGyRoomModalOpen, setIsGyRoomModalOpen] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    ScrollTrigger.create({
+      trigger: document.body,
+      start: 'top 80%',
+      onEnter: () => {
+        navigate('/night');
+      },
+    });
+  }, [navigate]);
 
   const handleGyIslandClick = () => {
     setIsGyRoomModalOpen(true);
